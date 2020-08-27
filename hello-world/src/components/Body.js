250a92967810c98685f9ef176350b89f4af94e37
sncandiani <sofiancandiani@gmail.com>
2020-08-26 21:16:38 -0500

    created body text and navbar for app

---
+++ hello-world/src/components/Body.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Body = () => {
+    return ( 
+        <div class="bodyText">
+        <div class="bodySect">
+            <img class="bodyImg" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.hdwallpaper.nu%2Fwp-content%2Fuploads%2F2015%2F04%2Fseoul-30385-1920x1200.jpg&f=1&nofb=1"></img>
+            <h3>Seoul, City of Wonder</h3>
+            <p>Seoul, the capital of South Korea, is a city that has an official history of 600 years. It is famous for many reasons. It is considered the historical center of the country, according to the Seoul Metropolitan Government. The city was previously called Hanyang and Gyeongseongbu, 
+                then renamed Seoul in 1945, when it was freed from Japanese occupation.</p>
+        </div>
+        </div>
+
+    )
+
+}
+
+export default Body
---
+++ hello-world/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Navbar = () => {
+    return ( 
+        <div class="nav">
+        <a href="http://localhost:3000/">Seoul</a>
+        <a href="http://localhost:3000/visit">Visit </a>
+        <a href="http://localhost:3000/learn-more">Learn more</a> 
+        </div>
+    )
+}
+
+export default Navbar
